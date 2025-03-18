4451afd43390f956b96458552aef7a3ba67586ca
ashima-11 <ashimawalia12@gmail.com>
2025-03-18 12:21:20 +0530

    Add files via upload

---
+++ Personal Portfolio/script.js
@@ -0,0 +1,69 @@
+// toggle navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar=document.querySelector(".navbar");
+
+menuIcon.onclick=()=>{
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+}
+
+// scroll section active
+let section = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+window.onscroll=()=>{
+  section.forEach(sec=>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navlinks.forEach.apply(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*-' + id + ']').classList.add('active');
+      });
+    };
+  });
+}
+
+// sticky navbar
+let header = document.querySelector("header");
+header.classList.toggle("sticky",window.scrollY > 100);
+
+// remove toggle icon and  navbar
+menuIcon.classList.remove("fa-xmark");
+navbar.classList.remove("active");
+
+// scroll reveal
+ScrollReveal({ 
+  distance:'80px',
+  duration:2000,
+  delay:200,
+ });
+ ScrollReveal().reveal('.home-content, heading', { origin:'top'});
+ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:'bottom' });
+ ScrollReveal().reveal('.home-contact h1, .about-img', { origin:'left' });
+ ScrollReveal().reveal('.home-content p, about-content', { origin: 'right' });
+
+//  typed js
+const typed = new Typed('.multiple-text',{
+  strings:['Frontend Developer','Web Designer','MERN Stack Developer'],
+  typeSpeed:70,
+  backSpeed:70,
+  backDelay:1000,
+  loop:  true,
+})
+
+// light-mode
+const lightMode = document.getElementById("lightMode");
+lightMode.onclick = function(){
+  document.body.classList.toggle("light-theme");
+
+  if(document.body.classList.contains("light-theme")){
+    lightMode.src='images/dark-moon.png';
+  }
+  else{
+    lightMode.src='images/moon.png';
+  }
+}
